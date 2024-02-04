267d2ff8b2e41ae4bbcaa67f14cb832a58629de9
ianlabicani <labicaniian@gmail.com>
2024-02-04 20:21:19 +0800

    feat(routes): :recycle: change empty path redirctTo to expenses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'expenses',
+    loadComponent: () =>
+      import('./expenses/expenses.page').then((m) => m.ExpensesPage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'expenses',
     pathMatch: 'full',
-  },  {
-    path: 'expenses',
-    loadComponent: () => import('./expenses/expenses.page').then( m => m.ExpensesPage)
   },
-
 ];
