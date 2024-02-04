6359d2bf0d2e724655f0dd275e59c2572323b905
ianlabicani <labicaniian@gmail.com>
2024-02-04 20:36:52 +0800

    refactor(page):

--- src/app/expenses/expenses.page.ts
+++ src/app/expenses/expenses.page.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ExpensesService } from './data-access/expenses.service';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { add, close } from 'ionicons/icons';
+import { IExpense } from './data-access';
 
 @Component({
   selector: 'app-expenses',
@@ -10,4 +20,45 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule, ReactiveFormsModule],
 })
-export class ExpensesPage {}
+export class ExpensesPage {
+  expensesService = inject(ExpensesService);
+
+  fb = inject(FormBuilder);
+  form: FormGroup;
+
+  alertButtons = [
+    {
+      text: 'Okay',
+      role: 'okay',
+      handler: () => {},
+    },
+  ];
+  isAlertOpen: boolean = false;
+
+  constructor() {
+    addIcons({ add, close });
+    this.form = this.fb.group({
+      reason: new FormControl(null, [Validators.required]),
+      amount: new FormControl(null, [Validators.min(1), Validators.required]),
+    });
+  }
+
+  onAdd() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      this.setOpen(true);
+      return;
+    }
+    const newExpense: IExpense = this.form.value;
+    this.expensesService.add(newExpense);
+    this.form.reset();
+  }
+
+  cancel() {
+    this.form.reset();
+  }
+
+  setOpen(arg0: boolean) {
+    this.isAlertOpen = arg0;
+  }
+}
